48ab6c32c9b6002a234f55ae28bcf9e0e4962945
Filip Kucera <kucerafilip@gmail.com>
2017-04-01 14:07:44 +0200

    generics - upper-bounded wildcard

--- src/main/java/cz/fku/generics/wrapper/RunWrapper.java
+++ src/main/java/cz/fku/generics/wrapper/RunWrapper.java
@@ -19,7 +19,7 @@ public class RunWrapper {
         rawWrapper.set(3);
         Integer integer = (Integer)rawWrapper.get();
 
-        //UNBOUNDED WILDCARD
+        //UNBOUNDED WILDCARD <?>
         Wrapper<?> unknownWrapper = new Wrapper<String>("some string");
         //can be assigned only to object or casted sice we do not know what it is (similat to raw type)
         Object o = unknownWrapper.get();
@@ -27,5 +27,20 @@ public class RunWrapper {
         WrapperUtil.printDetails(unknownWrapper);
         //can set anything except null
         unknownWrapper.set(null);
+
+        //UPPER-BOUNDED WILDCARD <? extends T>
+        Wrapper<? extends Number> numberWrapperInt = new Wrapper<Integer>(1);
+        Wrapper<? extends Number> numberWrapperLong = new Wrapper<Long>(1L);
+        Number number1 = numberWrapperLong.get();
+        Number number2 = numberWrapperLong.get();
+        WrapperUtil.sum(numberWrapperLong,numberWrapperLong);
+//        intWrapper = numberWrapperInt;
+        numberWrapperLong=integerWrapper;
+        //can get but cannot set
+        //When you try to use the set() method on numberWrapper, the compiler starts complaining
+        //because it cannot make sure at compile time that numberWrapper is a type of Integer or Double,
+        //which are subtypes of a Number.
+//        numberWrapperInt.set(1L);
+//        numberWrapperInt.set(1);
     }
 }
--- src/main/java/cz/fku/generics/wrapper/WrapperUtil.java
+++ src/main/java/cz/fku/generics/wrapper/WrapperUtil.java
@@ -6,6 +6,8 @@ package cz.fku.generics.wrapper;
 
 //For a generic type, a wildcard type is what an Object type is for a raw type.
 public class WrapperUtil {
+
+    //UNBOUNDED WILDCARD <?>
     public static void printDetails(Wrapper<?> wrapper) {
         // Can assign get() return value to Object
         Object value = wrapper.get();
@@ -17,5 +19,15 @@ public class WrapperUtil {
 
         System.out.println("Class: " + className);
         System.out.println("Value: " + value);
+    }
+
+    //UPPER-BOUNDED WILDCARD <? extends T>
+//    <? extends T> means anything that is of type T or its subclass is acceptable.
+    public static double sum(Wrapper<? extends Number> n1, Wrapper<? extends Number> n2){
+        Number num1 = n1.get();
+        Number num2 = n2.get();
+        double sum = num1.doubleValue() + num2.doubleValue();
+        System.out.println("SUM: " + sum);
+        return sum;
     }
 }
