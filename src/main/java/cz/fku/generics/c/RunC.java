205dd0131c34c3c7ebc46814d680035e54d4ff5f
Filip Kucera <kucerafilip@gmail.com>
2017-04-02 13:31:29 +0200

    assertion

--- src/main/java/cz/fku/generics/c/RunC.java
+++ src/main/java/cz/fku/generics/c/RunC.java
@@ -1,5 +1,8 @@
 package cz.fku.generics.c;
 
+import com.sun.istack.internal.NotNull;
+import org.springframework.util.Assert;
+
 /**
  * Created by Filip on 01.04.2017.
  */
@@ -7,5 +10,13 @@ public class RunC {
     public static void main(String[] args){
         C1 c = new C2();
         c.m1(10);  // Which method is called - C1.m() or C2.m2()?
+
+//        annotationTest(null);
+    }
+
+    public static void annotationTest(@NotNull String s){
+        assert s != null;
+        Assert.notNull(s);
+        s.concat("aaa");
     }
 }
